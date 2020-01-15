04c1c0802acfd9eefdc96c4a0a7eab7d38ad51ba
Noah <nkdotson@mix.wvu.edu>
2020-01-15 16:13:22 -0500

    Noah Dotson Task 01 upload b

--- task01.r
+++ task01.r
@@ -1,2 +1,30 @@
 install.packages("swirl")
 library(swirl)
+swirl()
+Noah Dotson
+1
+1
+1
+1
+5 + 7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+1
+my_sqrt
+my_div <- z / my_sqrt
+3
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z * 2 + 1000
+my_div
+2
+0
