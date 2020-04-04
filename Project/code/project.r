5ebf963f456f133030abb7ff364ae182c446e3ee
Noah <nkdotson@mix.wvu.edu>
2020-04-03 22:45:23 -0400

    Noah Dotson Project upload 2

--- Project/code/project.r
+++ Project/code/project.r
@@ -2,3 +2,12 @@ setwd("C:\\Users\\PCUSER\\Desktop\\Evolution\\Tasks\\Project\\data")
 MothData <- read.csv(file = "Appendix_Raw_Winglength.csv")
 head(MothData)
 nrow(MothData)
+#added genus/species names to data based on other data in the collection.
+plot?
+plot(MothData$FWL, MothData$Elevation, type="p", col="blue")
+Line <- lm(MothData$Elevation ~ MothData$FWL)
+abline(Line, col="red")
+#First graph completed
+#Winglength vs Elevation
+
+
